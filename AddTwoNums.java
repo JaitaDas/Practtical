9d7b8f9f986cc1ef94484781f3fe9469bb0db147
JaitaDas <107671153+JaitaDas@users.noreply.github.com>
2022-06-24 11:50:58 +0530

    Add files via upload

---
+++ AddTwoNums.java
@@ -0,0 +1,11 @@
+class AddTwoNums
+{
+  public static void main(String args[]){
+          int a=60;
+          int b=25;
+          System.out.println(a+b);
+          System.out.println(a-b); 
+          System.out.println(a*b); 
+          System.out.println(a/b);
+}
+}
