241e59c475d5daee2fc553a3c41bfec2abc9ed2e
JaitaDas <107671153+JaitaDas@users.noreply.github.com>
2022-06-24 12:02:38 +0530

    Update AddTwoNums.java

--- AddTwoNums.java
+++ AddTwoNums.java
@@ -3,9 +3,9 @@ class AddTwoNums
   public static void main(String args[]){
           int a=60;
           int b=25;
-          System.out.println(a+b);
-          System.out.println(a-b); 
-          System.out.println(a*b); 
-          System.out.println(a/b);
+          System.out.println(a+b);      //to print the addition value
+          System.out.println(a-b);      // to print the substraction value    
+          System.out.println(a*b);      // to print the multiplication value
+          System.out.println(a/b);      // to print the division value
 }
 }
