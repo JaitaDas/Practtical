af86c5ca176555815999dc9d18d266f9d407c487
JaitaDas <107671153+JaitaDas@users.noreply.github.com>
2022-06-24 12:57:40 +0530

    Update Abc.java

--- Abc.java
+++ Abc.java
@@ -1,8 +1,8 @@
 class Abc{
    public static void main (String[]args){
-    System.out.println("Name :Jaita Das");
-   System.out.println("Course : Java development");
- System.out.println("Stream : BCA");
+    System.out.println("Name :Jaita Das");              //to print the name 
+   System.out.println("Course : Java development");     //to print the course
+ System.out.println("Stream : BCA");                    //to print the stream
    
 }
 
