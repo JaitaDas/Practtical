239051e15063bb2d7f572db318e4aceefcda22f9
JaitaDas <107671153+JaitaDas@users.noreply.github.com>
2022-06-24 13:02:42 +0530

    Update Ajava.java

--- Ajava.java
+++ Ajava.java
@@ -1,7 +1,7 @@
 class Ajava{
    public static void main (String[]args){
-    System.out.println("Welcome to coading world.");
-   System.out.println("Hello World");
+    System.out.println("Welcome to coading world.");          //to print Welcome to coading world
+   System.out.println("Hello World");                        //to print Hello world
 }
 
 
