12561d94e38911ceb679a8d07eb23c044aadb0c2
JaitaDas <107671153+JaitaDas@users.noreply.github.com>
2022-06-24 12:54:48 +0530

    Add files via upload

---
+++ Abc.java
@@ -0,0 +1,10 @@
+class Abc{
+   public static void main (String[]args){
+    System.out.println("Name :Jaita Das");
+   System.out.println("Course : Java development");
+ System.out.println("Stream : BCA");
+   
+}
+
+
+}
