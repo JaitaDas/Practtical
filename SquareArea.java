1c5926f88b95915a36433fc128f24b8b3eb554ba
JaitaDas <107671153+JaitaDas@users.noreply.github.com>
2022-06-29 21:18:23 +0530

    Update SquareArea.java

--- SquareArea.java
+++ SquareArea.java
@@ -1,7 +1,7 @@
-public class SquareArea
+public class SquareArea          //creating SquareArea class
 {
 	float side, area;
-    void insert(float s, float a) {
+    void insert(float s, float a) {    //method
       side=s; 
       area=a;
       }
@@ -9,9 +9,9 @@ public class SquareArea
 	  
      System.out.print(area=4*side);
   }
-      class SquareArea1{
+      class SquareArea1{            //creating another class SquareArea1
     	  public static void main(String[] args) {
-    		  SquareArea r1=new SquareArea();
+    		  SquareArea r1=new SquareArea();    //creating an object   
     		  r1.insert(10.5f,5.2f);
     		  r1.calculateArea();
     	  }
