aff189b9dc59a6be65acbc22e854eab83c66c5f3
JaitaDas <107671153+JaitaDas@users.noreply.github.com>
2022-06-29 21:25:50 +0530

    Update RectangleParameter.java

--- RectangleParameter.java
+++ RectangleParameter.java
@@ -1,5 +1,5 @@
-
-public class RectangleParameter {
+//Defining a class RectangleParameter
+public class RectangleParameter {   
 	int length;
 	int width;
 	void insert(int l,int w) {
@@ -10,9 +10,9 @@ public class RectangleParameter {
 	   System.out.println(2*(length+width)); 
 	   }
    
-static class RectangleParameter1{
+static class RectangleParameter1{      //Defining another class RectangleParameter1
 	public static void main(String args[]) {
-  RectangleParameter r1=new RectangleParameter();
+  RectangleParameter r1=new RectangleParameter();   //creating an object
   r1.insert(11,5);
   r1.calculateParameter();
  
