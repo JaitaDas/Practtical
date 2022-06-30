6683ebad45ffa4a48e04263cd4fa318042e27e37
JaitaDas <107671153+JaitaDas@users.noreply.github.com>
2022-06-30 11:31:01 +0530

    Update RectangleArea1.java

--- RectangleArea1.java
+++ RectangleArea1.java
@@ -1,18 +1,21 @@
+//Defining a class
 public class RectangleArea1 {
+//Defining fields	
 	int length;
 	int width;
 	void insert(int l,int w) {
 		length=l;
 		width=w;
 		}
+	//Defining another class
    void calculateArea1() {
 	   System.out.println(length*width); 
 	   }
    
  static class TestRectangle1{
 	public static void main(String args[]) {
-  RectangleArea1 r1=new RectangleArea1();
-  RectangleArea1 r2=new RectangleArea1();
+  RectangleArea1 r1=new RectangleArea1();   //Creating an object
+  RectangleArea1 r2=new RectangleArea1();  //Creating an object
 
   r1.insert(11,5);
   r2.insert(3,15);
