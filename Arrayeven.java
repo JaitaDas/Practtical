a1450d4a78939cb22ece95b01073c49e68ade191
JaitaDas <107671153+JaitaDas@users.noreply.github.com>
2022-07-19 13:56:08 +0530

    Update Arrayeven.java

--- Arrayeven.java
+++ Arrayeven.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 public class Arrayeven
-{
+{    //class
     public static void main(String[] args) 
     {
         int n;
         Scanner s = new Scanner(System.in);
         System.out.print("Enter no. of array elements:");
-        n = s.nextInt();
+        n = s.nextInt();   
         int a[] = new int[n];
         System.out.println("Enter all the elements:");
-        for (int i = 0; i < n; i++) 
+        for (int i = 0; i < n; i++) //logic for print
         {
             a[i] = s.nextInt();
         }
